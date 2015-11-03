d588568f39afd73c81905297dcdc95e5570e62a0
Rirax <rlechapt@student.42.fr>
2015-11-03 18:04:07 +0100

    Update fonctions.py

--- tmp/fonctions.py
+++ tmp/fonctions.py
@@ -1,20 +1,5 @@
 import re
 
-# def init_tab_noequal(argument):
-# 	p = re.compile(r'([\dX^]*)[ ]*([+-])*')
-# 	b = p.findall(argument)
-# 	print b
-# 	i = 0
-# 	tab = []
-# 	print b[0]
-# 	while i < len(b):
-# 		# if b[i].isnumeric() and b[i + 1] != 'X'
-# 		# 	tab.append[b[i], 0]
-# 		i += 1
-# 	print tab
-
-
-
 def init_tab(argument):
 	p = re.compile(r'(.*) = (.*)')
 	b = p.findall(argument)
@@ -72,29 +57,7 @@ def init_tab(argument):
 	tab = tab1 + tab
 	return tab
 
-
-def calculate_coord(tab):
-	coord = []
-	i = 0
-	while i <= 2:
-		if float(tab[i][1]) == 0:
-			zero = float(tab[i][0])
-		elif float(tab[i][1]) == 1:
-			un = float(tab[i][0])
-		elif float(tab[i][1]) == 2:
-			deux = float(tab[i][0])
-		i += 1
-	x = -20
-	while x < 20:
-		y = (x * x * deux) + (x * un) + zero
-		a = [x, y]
-		coord.append(a)
-		x += 2
-	return coord
-
-
 def check_argument(arg):
-	"""if re.search(r'[\w\*\+\-\s\^\.]+=[\w\*\+\-\s\^\.]+', arg):"""
 	if re.search(r'([+\- \d.]*) \* ([X^\d]*)', arg):
 		return 1
 	else:
